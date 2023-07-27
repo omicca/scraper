cefc8171d0d7182a391321b6ccc6aaede782e4ef
Oliver Michaelsen <56824279+omicca@users.noreply.github.com>
2023-07-27 04:12:25 +0200

    email scrape test

--- scraper.py
+++ scraper.py
@@ -1,5 +1,15 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as soup
+import re
 
 with open("example.html", "r") as f:
-    document = BeautifulSoup(f, "html.parser")
-    
+    document = soup(f, "html.parser")
+
+tags = document.find_all('a')
+
+emails = []
+for email in tags:
+    email_regex = re.findall("[a-z]+@[a-z]+.[a-z]+", email.string)
+    emails += email_regex
+
+
+print(emails)
