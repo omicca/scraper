5bf6d78232a2d5c448a5043e7cb5e5d985d77dde
Oliver Michaelsen <56824279+omicca@users.noreply.github.com>
2023-07-31 03:18:31 +0200

    extract prices to nested dict

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from scrape import extract_email, convert_to_csv, extract_prices
 
-test = extract_prices('https://www.pricerunner.dk/deals')
-for item in test:
-    print(item)
+title = extract_prices('https://www.pricerunner.dk/deals')
+
--- scrape.py
+++ scrape.py
@@ -18,17 +18,18 @@ def extract_email():
     tags = document.find_all('a')
 
     emails = []
+    email_filter = r'(?<=mailto:)\s*([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})'
     for email in tags:
         href = email.get('href', "")
         if href.startswith('mailto:'):
-            email_regex = re.findall(r'(?<=mailto:)\s*([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})', href, re.DOTALL)
+            email_regex = re.findall(email_filter, href, re.DOTALL)
             if email_regex:
                 emails += email_regex
 
     return emails
 
 def extract_prices(url):
-    """Extracts recent prices from"""
+    """Extracts prices of deals from pricerunner"""
     site_prices = requests.get(url)
     document = soup(site_prices.text, "html.parser")
     
@@ -41,7 +42,14 @@ def extract_prices(url):
     for pric in document.find_all('span', class_='pr-1859nm3'):
         final_price += pric
 
-    return final_title, final_price
+    final = {}
+    i = 0
+    for title, price in zip(final_title, final_price):
+        final[f'{i}'] = {}
+        final[f'{i}'][title] = price
+        i += 1
+
+    return final
 
 
 def convert_to_csv(data_list):
