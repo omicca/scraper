41dfda9f65ca8114937a38bc6df59e17f6e2a16c
Oliver Michaelsen <omicha19@student.aau.dk>
2023-08-06 19:27:24 +0200

    gitignore
    added gitignore .idea .json to avoid unnecessary uploads

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from scrape import extract_email, convert_to_csv, extract_prices, convert_to_json
 
-title = extract_prices('https://www.pricerunner.dk/deals')
+emails = extract_email()
 
-convert_to_json(title)
+print(emails)
